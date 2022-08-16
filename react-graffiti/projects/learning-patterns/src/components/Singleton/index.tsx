61d0eb866dc0ad8d25e0918e7c4c86cda40dd038
hujeong <hujeong@suresofttech.com>
2022-08-16 14:45:00 +0900

    add menu select

---
+++ react-graffiti/projects/learning-patterns/src/components/Singleton/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Singleton() {
+    return <div>Singlton</div>;
+}
--- react-graffiti/projects/learning-patterns/src/components/index.tsx
+++ react-graffiti/projects/learning-patterns/src/components/index.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { MenuItem, Select } from '@mui/material';
+import Singleton from './Singleton';
 
 export default function LearningPatterns() {
-    return <div>learning patterns</div>;
+    const [selectedComponent, setSelectedComponent] = React.useState('');
+
+    return (
+        <div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <p style={{ marginRight: '12px' }}> select pattern : </p>
+                <Select style={{ height: '30px' }} value={selectedComponent} onChange={(e) => setSelectedComponent(e.target.value)}>
+                    <MenuItem value="Singleton">Singleton</MenuItem>
+                </Select>
+            </div>
+            <div>{selectedComponent === 'Singleton' && <Singleton />}</div>
+        </div>
+    );
 }
